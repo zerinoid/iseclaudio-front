d1d92a891329f6168892b3e7f2524337fa841980
zerinoid <leo.zerino@gmail.com>
2024-10-16 04:43:53 -0300

    feat: fully responsive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Image from "next/image";
 import styles from "./Header.module.css";
 import Link from "next/link";
+import logo from "#/ise_logo.svg";
+import instagram from "#/instagram.svg";
 
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -13,15 +15,13 @@ const Header = () => {
   };
 
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} container`}>
       <div className={styles.logo}>
         <Link href="/">
-          <Image src="/ise_logo.svg" alt="Logo" width={26} height={40} />
+          <Image src={logo} alt="Logo" />
         </Link>
       </div>
-      <nav
-        className={`${styles.nav} ${isMobileMenuOpen ? styles.mobileOpen : ""}`}
-      >
+      <nav className={`${styles.nav}`}>
         <button
           className={styles.hamburger}
           aria-label="Toggle menu"
@@ -30,33 +30,52 @@ const Header = () => {
           onClick={toggleMobileMenu}
         >
           <div
-            className={`${styles.bar} ${isMobileMenuOpen ? styles.barOpen : ""}`}
+            className={`${styles.bar} ${
+              isMobileMenuOpen ? styles.barOpen : ""
+            }`}
           ></div>
           <div
-            className={`${styles.bar} ${isMobileMenuOpen ? styles.barOpen : ""}`}
+            className={`${styles.bar} ${
+              isMobileMenuOpen ? styles.barOpen : ""
+            }`}
           ></div>
           <div
-            className={`${styles.bar} ${isMobileMenuOpen ? styles.barOpen : ""}`}
+            className={`${styles.bar} ${
+              isMobileMenuOpen ? styles.barOpen : ""
+            }`}
           ></div>
         </button>
-        <ul className={styles.navList}>
+        <ul
+          className={`${styles.navList} ${
+            isMobileMenuOpen ? styles.mobileOpen : ""
+          }`}
+        >
           <li className={styles.navItem}>
             <Link href="/">HOME</Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/about">BIO</Link>
+            <Link href="/bio">BIO</Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/services">EXHIBTIONS</Link>
+            <Link href="/exhibtions">EXHIBTIONS</Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/contact">PROJECTS</Link>
+            <Link href="/projects">PROJECTS</Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/contact">SUBSCRIBE</Link>
+            <Link href="/subscribe">SUBSCRIBE</Link>
           </li>
           <li className={styles.navItem}>
             <Link href="/contact">CONTACT</Link>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/ise.claudio/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image alt="Instagram" src={instagram} />
+            </a>
           </li>
         </ul>
       </nav>
