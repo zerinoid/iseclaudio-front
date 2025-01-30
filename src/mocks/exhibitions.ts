888ac381b2b77083cf5740a255940b85fdba0d7b
zerinoid <leo.zerino@gmail.com>
2025-01-30 17:02:08 -0300

    feat: mock pictures

--- src/mocks/exhibitions.ts
+++ src/mocks/exhibitions.ts
@@ -5,16 +5,35 @@ const exhibitions: Work[] = [
     name: 'Lorem Ipsum Dolor Sit Amet',
     id: 1,
     images: [
-      '/temp/img1.jpg',
-      '/temp/img2.jpg',
-      '/temp/img3.jpg',
-      '/temp/img4.jpg',
-      '/temp/img5.jpg',
-      '/temp/img6.jpg'
+      '/exhib1/img1.jpg',
+      '/exhib1/img2.jpg',
+      '/exhib1/img3.jpg',
+      '/exhib1/img4.jpg',
+      '/exhib1/img5.jpg',
+      '/exhib1/img6.jpg'
     ]
   },
-  { name: 'Consectetur Adipiscing Elit', id: 2, images: [] },
-  { name: 'Sed Do Eiusmod', id: 3, images: [] },
+  {
+    name: 'Consectetur Adipiscing Elit',
+    id: 2,
+    images: [
+      '/exhib2/img1.jpg',
+      '/exhib2/img2.jpg',
+      '/exhib2/img3.jpg',
+      '/exhib2/img4.png',
+      '/exhib2/img5.jpg'
+    ]
+  },
+  {
+    name: 'Sed Do Eiusmod',
+    id: 3,
+    images: [
+      '/exhib3/img1.png',
+      '/exhib3/img2.png',
+      '/exhib3/img3.jpg',
+      '/exhib3/img4.jpg'
+    ]
+  },
   { name: 'Tempor Incididunt', id: 4, images: [] },
   { name: 'Ut Labore', id: 5, images: [] }
 ]
